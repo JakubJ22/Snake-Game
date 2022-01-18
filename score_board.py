f30d167cccbe9373a1766b1294d6135568487759
JakubJ22 <jakub.jarzembowski2@gmail.com>
2022-01-18 19:14:53 +0100

    Added best score

--- score_board.py
+++ score_board.py
@@ -2,6 +2,8 @@ from turtle import Turtle
 import time
 FONT = ('Arial', 15, 'normal')
 
+best_score_file = "best_score.txt"
+
 
 class Score_Board(Turtle):
     """An object of this class shows a score of our game on upper part of the screen"""
@@ -14,7 +16,7 @@ class Score_Board(Turtle):
         self.hideturtle()
         self.penup()
         self.goto(0, 280)
-        
+        self.max_score = self.what_is_max_score()
 
     def give_score(self):
         self.clear()
@@ -33,11 +35,29 @@ class Score_Board(Turtle):
 
     def exit_info(self):
         self.goto(0, -18)
-        self.write("Click on the screen to exit", align="center",
-                   font=('Arial', 7, 'normal'))
+        self.write("Click Space to start a new game or Esc to close the window.", align="center",
+                   font=('Arial', 9, 'normal'))
 
     def game_over(self):
         self.home()
         self.write("GAME OVER", align="center",
                    font=FONT)
         self.exit_info()
+        self.update_best_score()
+        self.give_best_score()
+
+    def update_best_score(self):
+        if self.score > self.max_score:
+            self.max_score = self.score
+
+            with open(best_score_file, "w") as file:
+                file.write(str(self.max_score))
+
+    def give_best_score(self):
+        self.goto(-20, -40)
+        self.write(f"Best score: {self.max_score}")
+
+    def what_is_max_score(self):
+        with open(best_score_file, "r") as file:
+            data = file.read()
+            return int(data)
